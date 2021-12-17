55e62f72f0259bd2510b404248160b7f3f3f8756
splonka98 <szymon.plonka@microsoft.wsei.edu.pl>
2021-12-17 20:03:50 +0100

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PupiloSzamak.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
